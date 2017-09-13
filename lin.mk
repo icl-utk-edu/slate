b59d58e28d3994ab049a9305c40e82e5f320411f
Jakub Kurzak <kurzak@icl.utk.edu>
2017-09-13 17:20:43 -0400

    proper lin.mk

--- lin.mk
+++ lin.mk
@@ -1,17 +1,16 @@
 
-CC  = gcc
-CPP = g++
+CC  = mpicc
+CPP = mpicxx
 
 CFLAGS  = -O3 -std=c99
-CCFLAGS = -O3 -std=c++11 -fopenmp 
+CCFLAGS = -O3 -std=c++17 -fopenmp 
 
+#MPI  = /sw/openmpi/2.1.1-thread-multiple
 CUDA = /usr/local/cuda
-MPI  = /sw/openmpi/2.1.1-thread-multiple
 
 INC = -I$(MPI)/include \
       -I$(MKLROOT)/include
 
-LIB = -L$(MPI)/lib -lmpi \
-      -L${MKLROOT}/lib \
-      -lmkl_intel_lp64 -lmkl_sequential -lmkl_core -lpthread -lm -ldl \
-      -L$(CUDA)/lib64 -lcublas
+LIB = -L${MKLROOT}/lib -lmkl_intel_lp64 -lmkl_sequential -lmkl_core -lpthread -lm -ldl \
+      -L$(CUDA)/lib64 -lcublas -lcudart \
+      -lmpi

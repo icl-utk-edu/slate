f053fdc2a9d830499f97ac03d371c197e3845102
Ali Charara <ali@icl.utk.edu>
2019-12-11 14:22:20 -0500

    gemm: target::device uses the internal::gemmPrep & internal::gemmExec for better scheduling of kernel executions

--- src/gemm.cc
+++ src/gemm.cc
@@ -39,6 +39,8 @@
 
 #include "slate/slate.hh"
 #include "internal/internal.hh"
+#include "internal/internal_batch.hh"
+#include "aux/Debug.hh"
 
 #include <list>
 #include <tuple>
@@ -184,6 +186,189 @@ void gemm(slate::internal::TargetType<target>,
                     alpha,         A.sub(0, A.mt()-1, k, k),
                                    B.sub(k, k, 0, B.nt()-1),
                     scalar_t(1.0), std::move(C),
+                    layout);
+            }
+        }
+        #pragma omp taskwait
+        C.tileUpdateAllOrigin();
+    }
+
+    C.releaseWorkspace();
+}
+
+//------------------------------------------------------------------------------
+/// @internal
+/// Distributed parallel general matrix-matrix multiplication.
+/// Device target implementation.
+/// Dependencies enforce the following behavior:
+/// - bcast communications are serialized,
+/// - gemm operations are serialized,
+/// - bcasts can get ahead of gemms by the value of lookahead.
+/// ColMajor layout is assumed
+///
+/// @ingroup gemm_specialization
+///
+template <typename scalar_t>
+void gemm(slate::internal::TargetType<Target::Devices>,
+          scalar_t alpha, Matrix<scalar_t>& A,
+                          Matrix<scalar_t>& B,
+          scalar_t beta,  Matrix<scalar_t>& C,
+          int64_t lookahead)
+{
+    using namespace blas;
+    using BcastList = typename Matrix<scalar_t>::BcastList;
+
+    // Assumes column major
+    const Layout layout = Layout::ColMajor;
+
+    // OpenMP needs pointer types, but vectors are exception safe
+    std::vector<uint8_t> bcast_vector(A.nt());
+    std::vector<uint8_t>  gemm_vector(A.nt());
+    uint8_t* bcast = bcast_vector.data();
+    uint8_t* gemm  =  gemm_vector.data();
+
+    std::vector< GemmBatchArrays<scalar_t> > gemmBatch(
+                    lookahead+1,
+                    GemmBatchArrays<scalar_t>(A.num_devices()));
+
+    #pragma omp parallel
+    #pragma omp master
+    {
+        omp_set_nested(1);
+
+        C.reserveDeviceWorkspace();
+
+        // send first block col of A and block row of B
+        #pragma omp task depend(out:bcast[0])
+        {
+            // broadcast A(i, 0) to ranks owning block row C(i, :)
+            #pragma omp task default(shared)
+            {
+                BcastList bcast_list_A;
+                for (int64_t i = 0; i < A.mt(); ++i)
+                    bcast_list_A.push_back({i, 0, {C.sub(i, i, 0, C.nt()-1)}});
+                A.template listBcast<Target::Devices>(bcast_list_A, layout);
+            }
+
+            // broadcast B(0, j) to ranks owning block col C(:, j)
+            #pragma omp task default(shared)
+            {
+                BcastList bcast_list_B;
+                for (int64_t j = 0; j < B.nt(); ++j)
+                    bcast_list_B.push_back({0, j, {C.sub(0, C.mt()-1, j, j)}});
+                B.template listBcast<Target::Devices>(bcast_list_B, layout);
+            }
+            #pragma omp taskwait
+
+            // prepare first internal::gemm pointer's arrays and prefetch its data
+            internal::gemmPrep<Target::Devices>(
+                    alpha, A.sub(0, A.mt()-1, 0, 0),
+                           B.sub(0, 0, 0, B.nt()-1),
+                    beta,  std::move(C),
+                    std::move(&gemmBatch[0]),
+                    layout);
+        }
+
+        // send next lookahead block cols of A and block rows of B
+        for (int64_t k = 1; k < lookahead+1 && k < A.nt(); ++k) {
+            #pragma omp task depend(in:bcast[k-1]) \
+                             depend(out:bcast[k])
+            {
+                // broadcast A(i, k) to ranks owning block row C(i, :)
+                #pragma omp task default(shared)
+                {
+
+                    BcastList bcast_list_A;
+                    for (int64_t i = 0; i < A.mt(); ++i)
+                        bcast_list_A.push_back({i, k, {C.sub(i, i, 0, C.nt()-1)}});
+                    A.template listBcast<Target::Devices>(bcast_list_A, layout);
+                }
+
+                // broadcast B(k, j) to ranks owning block col C(:, j)
+                #pragma omp task default(shared)
+                {
+                    BcastList bcast_list_B;
+                    for (int64_t j = 0; j < B.nt(); ++j)
+                        bcast_list_B.push_back({k, j, {C.sub(0, C.mt()-1, j, j)}});
+                    B.template listBcast<Target::Devices>(bcast_list_B, layout);
+                }
+                #pragma omp taskwait
+
+                // prepare lookahead internal::gemm pointer's arrays
+                // data is already prefetched
+                internal::gemmPrep<Target::Devices>(
+                        alpha,         A.sub(0, A.mt()-1, k, k),
+                                       B.sub(k, k, 0, B.nt()-1),
+                        scalar_t(1.0), std::move(C),
+                        std::move(&gemmBatch[k % (lookahead+1)]),
+                        layout, true);
+            }
+        }
+
+        // multiply alpha A(:, 0) B(0, :) + beta C
+        #pragma omp task depend(in:bcast[0]) \
+                         depend(out:gemm[0])
+        {
+            internal::gemmExec<Target::Devices>(
+                    alpha, A.sub(0, A.mt()-1, 0, 0),
+                           B.sub(0, 0, 0, B.nt()-1),
+                    beta,  std::move(C),
+                    std::move(&gemmBatch[0]),
+                    layout);
+        }
+
+        for (int64_t k = 1; k < A.nt(); ++k) {
+
+            // send next block col of A and block row of B
+            if (k+lookahead < A.nt()) {
+                #pragma omp task depend(in:gemm[k-1]) \
+                                 depend(in:bcast[k+lookahead-1]) \
+                                 depend(out:bcast[k+lookahead])
+                {
+                    // broadcast A(i, k+la) to ranks owning block row C(i, :)
+                    #pragma omp task default(shared)
+                    {
+                        BcastList bcast_list_A;
+                        for (int64_t i = 0; i < A.mt(); ++i) {
+                            bcast_list_A.push_back(
+                                {i, k+lookahead, {C.sub(i, i, 0, C.nt()-1)}});
+                        }
+                        A.template listBcast<Target::Devices>(bcast_list_A, layout);
+                    }
+
+                    // broadcast B(k+la, j) to ranks owning block col C(:, j)
+                    #pragma omp task default(shared)
+                    {
+                        BcastList bcast_list_B;
+                        for (int64_t j = 0; j < B.nt(); ++j) {
+                            bcast_list_B.push_back(
+                                {k+lookahead, j, {C.sub(0, C.mt()-1, j, j)}});
+                        }
+                        B.template listBcast<Target::Devices>(bcast_list_B, layout);
+                    }
+                    #pragma omp taskwait
+
+                    // prepare lookahead internal::gemm pointer's arrays
+                    // data is already prefetched
+                    internal::gemmPrep<Target::Devices>(
+                        alpha,         A.sub(0, A.mt()-1, k+lookahead, k+lookahead),
+                                       B.sub(k+lookahead, k+lookahead, 0, B.nt()-1),
+                        scalar_t(1.0), std::move(C),
+                        std::move(&gemmBatch[(k+lookahead) % (lookahead+1)]),
+                        layout, true);
+                }
+            }
+
+            // multiply alpha A(:, k) B(k, :) + C, no beta
+            #pragma omp task depend(in:bcast[k]) \
+                             depend(in:gemm[k-1]) \
+                             depend(out:gemm[k])
+            {
+                internal::gemmExec<Target::Devices>(
+                    alpha,         A.sub(0, A.mt()-1, k, k),
+                                   B.sub(k, k, 0, B.nt()-1),
+                    scalar_t(1.0), std::move(C),
+                    std::move(&gemmBatch[k % (lookahead+1)]),
                     layout);
             }
         }

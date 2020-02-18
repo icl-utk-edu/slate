af05dbbdba9dda0d3dade0b08ef7492eb04fccb8
Dalal Sukkari <sukkari@icl.utk.edu>
2020-02-18 14:11:34 -0500

    remove unnecessary initialization

--- src/steqr2.cc
+++ src/steqr2.cc
@@ -78,7 +78,7 @@ void steqr2(slate::internal::TargetType<target>,
     int64_t info = 0;
     int64_t nrc, ldc;
 
-    int myrow = 0;
+    int myrow;
     int izero = 0;
     scalar_t zero = 0.0, one = 1.0;
 

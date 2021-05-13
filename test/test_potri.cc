1e3c723f22379fe9985d46d695a8a9c2171f3546
Paul Bagwell <pdbagwell@icl.utk.edu>
2021-05-13 18:47:58 -0400

    test_potri

--- test/test_potri.cc
+++ test/test_potri.cc
@@ -7,6 +7,7 @@
 #include "test.hh"
 #include "blas/flops.hh"
 #include "lapack/flops.hh"
+#include "print_matrix.hh"
 
 #include "scalapack_wrappers.hh"
 #include "scalapack_support_routines.hh"
@@ -17,7 +18,7 @@
 #include <cstdio>
 #include <cstdlib>
 #include <utility>
-
+//#define SLATE_HAVE_SCALAPACK
 //------------------------------------------------------------------------------
 template <typename scalar_t>
 void test_potri_work(Params& params, bool run)
@@ -27,8 +28,9 @@ void test_potri_work(Params& params, bool run)
     // get & mark input values
     slate::Uplo uplo = params.uplo();
     int64_t n = params.dim.n();
-    int64_t p = params.grid.m();
-    int64_t q = params.grid.n();
+    int64_t nrhs = params.nrhs();
+    int p = params.grid.m();
+    int q = params.grid.n();
     int64_t nb = params.nb();
     int64_t lookahead = params.lookahead();
     bool ref_only = params.ref() == 'o';
@@ -38,6 +40,8 @@ void test_potri_work(Params& params, bool run)
     int verbose = params.verbose(); SLATE_UNUSED(verbose);
     slate::Origin origin = params.origin();
     slate::Target target = params.target();
+    params.matrix.mark();
+    params.matrixB.mark();
 
     // mark non-standard output values
     params.time();
@@ -45,8 +49,11 @@ void test_potri_work(Params& params, bool run)
     params.ref_time();
     params.ref_gflops();
 
-    if (! run)
+    if (! run){
+        params.matrix.kind.set_default( "rand_dominant" );
+        params.matrixB.kind.set_default( "rand" );
         return;
+    }
 
     slate::Options const opts =  {
         {slate::Option::Lookahead, lookahead},
@@ -56,71 +63,45 @@ void test_potri_work(Params& params, bool run)
     int mpi_rank;
     MPI_Comm_rank(MPI_COMM_WORLD, &mpi_rank);
 
+    // constants
+    const scalar_t zero = 0;
+    const scalar_t one  = 1;
+
     // Local values
-    const int izero = 0, ione = 1;
+    int myrow, mycol;
+    gridinfo(mpi_rank, p, q, &myrow, &mycol);
 
-    // BLACS/MPI variables
-    int ictxt, nprow, npcol, myrow, mycol, info;
-    int descA_tst[9], descA_ref[9];
-    int descC_chk[9];
-    int iam = 0, nprocs = 1;
-    int iseed = 1;
-
-    // initialize BLACS and ScaLAPACK
-    Cblacs_pinfo(&iam, &nprocs);
-    slate_assert(p*q <= nprocs);
-    Cblacs_get(-1, 0, &ictxt);
-    Cblacs_gridinit(&ictxt, "Col", p, q);
-    Cblacs_gridinfo(ictxt, &nprow, &npcol, &myrow, &mycol);
-
-    // matrix A, figure out local size, allocate, create descriptor, initialize
-    int64_t mlocA = num_local_rows_cols(n, nb, myrow, nprow);
-    int64_t nlocA = num_local_rows_cols(n, nb, mycol, npcol);
-    scalapack_descinit(descA_tst, n, n, nb, nb, izero, izero, ictxt, mlocA, &info);
-    slate_assert(info == 0);
-    int64_t lldA = (int64_t)descA_tst[8];
-    std::vector<scalar_t> A_tst(lldA*nlocA);
-    scalapack_pplghe(&A_tst[0], n, n, nb, nb, myrow, mycol, nprow, npcol, mlocA, iseed + 1);
+    // matrix A, figure out local size, allocate
+    int64_t mlocA = num_local_rows_cols(n, nb, myrow, p);
+    int64_t nlocA = num_local_rows_cols(n, nb, mycol, q);
+    int64_t lldA  = blas::max(1, mlocA); // local leading dimension of A
+    std::vector<scalar_t> A_data(lldA*nlocA);
 
     // todo: work-around to initialize BaseMatrix::num_devices_
     slate::HermitianMatrix<scalar_t> A0(uplo, n, nb, p, q, MPI_COMM_WORLD);
 
-    // Setup SLATE matrix A based on Scalapack matrix and data in A_tst
+    // Setup SLATE matrix A based on Scalapack matrix and data in A_data
     slate::HermitianMatrix<scalar_t> A;
     if (origin == slate::Origin::Devices) {
         // Copy local ScaLAPACK data to GPU or CPU tiles.
         slate::Target origin_target = origin2target(origin);
-        A = slate::HermitianMatrix<scalar_t>(uplo, n, nb, nprow, npcol, MPI_COMM_WORLD);
+        A = slate::HermitianMatrix<scalar_t>(uplo, n, nb, p, q, MPI_COMM_WORLD);
         A.insertLocalTiles(origin_target);
-        copy(&A_tst[0], descA_tst, A);
     }
     else {
         // Create SLATE matrix from the ScaLAPACK layouts
-        A = slate::HermitianMatrix<scalar_t>::fromScaLAPACK(uplo, n, &A_tst[0], lldA, nb, nprow, npcol, MPI_COMM_WORLD);
+        A = slate::HermitianMatrix<scalar_t>::fromScaLAPACK(uplo, n, &A_data[0], lldA, nb, p, q, MPI_COMM_WORLD);
     }
 
-    // if check (or ref) is required, copy test data and create a descriptor for it
-    std::vector<scalar_t> A_ref;
+    slate::generate_matrix(params.matrix, A);
+
+    // if check (or ref) is required, copy test data
+    slate::HermitianMatrix<scalar_t> Aref;
+    std::vector<scalar_t> Aref_data;
     if (check || ref) {
-        A_ref = A_tst;
-        scalapack_descinit(descA_ref, n, n, nb, nb, izero, izero, ictxt, mlocA, &info);
-        slate_assert(info == 0);
-    }
-
-    // If check is required: keep the norm(A original); create C_chk = identity matrix to hold A*inv(A)
-    std::vector<scalar_t> C_chk;
-    real_t A_norm = 0.0;
-    if (check) {
-        // If check is required, record the norm of the original matrix
-        A_norm = slate::norm(slate::Norm::One, A);
-        // if check is required, create identity matrix to check the result of multiplying A and A_inv
-        // C_chk starts with the same size/dimensions as A_tst
-        C_chk = A_tst;
-        scalapack_descinit(descC_chk, n, n, nb, nb, izero, izero, ictxt, mlocA, &info);
-        slate_assert(info == 0);
-        // Make C_chk into an identity matrix
-        scalar_t zero = 0.0; scalar_t one = 1.0;
-        scalapack_plaset("All", n, n, zero, one, &C_chk[0], ione, ione, descC_chk);
+        Aref_data.resize( A_data.size() );
+        Aref = slate::HermitianMatrix<scalar_t>::fromScaLAPACK(uplo, n, &Aref_data[0], lldA, nb, p, q, MPI_COMM_WORLD);
+        slate::copy( A, Aref );
     }
 
     double gflop = 0.0;
@@ -133,11 +114,7 @@ void test_potri_work(Params& params, bool run)
         if (trace) slate::trace::Trace::on();
         else slate::trace::Trace::off();
 
-        {
-            slate::trace::Block trace_block("MPI_Barrier");
-            MPI_Barrier(MPI_COMM_WORLD);
-        }
-        double time = testsweeper::get_wtime();
+        double time = barrier_get_wtime(MPI_COMM_WORLD);
 
         //==================================================
         // Run SLATE test.
@@ -151,75 +128,128 @@ void test_potri_work(Params& params, bool run)
         // slate::potrf(A, opts);
         // slate::potri(A, opts);
 
-        {
-            slate::trace::Block trace_block("MPI_Barrier");
-            MPI_Barrier(MPI_COMM_WORLD);
-        }
-        double time_tst = testsweeper::get_wtime() - time;
+        time = barrier_get_wtime(MPI_COMM_WORLD) - time;
 
         if (trace) slate::trace::Trace::finish();
 
         // compute and save timing/performance
-        params.time() = time_tst;
-        params.gflops() = gflop / time_tst;
+        params.time() = time;
+        params.gflops() = gflop / time;
     }
 
+    // Test only with SLATE routines
     if (check) {
-        // Check  || I - inv(A)*A || / ( || A || * N ) <=  tol * eps
-
-        if (origin != slate::Origin::ScaLAPACK) {
-            // Copy SLATE result back from GPU or CPU tiles.
-            copy(A, &A_tst[0], descA_tst);
-        }
-
-        // to make the diagonal of A_ref real
-        slate::HermitianMatrix<scalar_t> Aref;
-        Aref = slate::HermitianMatrix<scalar_t>::fromScaLAPACK(
-                      uplo, n, &A_ref[0], lldA, nb, nprow, npcol, MPI_COMM_WORLD);
-        // make diagonal real
-        for (int64_t i = 0; i < Aref.nt(); ++i) {
-            if (Aref.tileIsLocal(i, i)) {
-                auto T = Aref(i, i);
-                for (int ii = 0; ii < Aref.tileMb(i); ++ii) {
-                    T.at(ii, ii) = std::real( T.at(ii, ii) );
-                }
-            }
-        }
-
-        // C_chk has been setup as an identity matrix; C_chk = C_chk - inv(A)*A
-        // A should have real diagonal. potrf and potri ignore the img part on the diagonal
-        scalar_t alpha = -1.0; scalar_t beta = 1.0;
-        scalapack_phemm("Left", uplo2str(uplo), n, n, alpha,
-                        &A_tst[0], ione, ione, descA_tst,
-                        &A_ref[0], ione, ione, descA_ref, beta,
-                        &C_chk[0], ione, ione, descC_chk);
-
-        // Norm of C_chk ( = I - inv(A) * A )
-        // allocate work space for lange and lanhe
-        int lcm = scalapack_ilcm(&nprow, &npcol);
-        int ldw = nb*slate::ceildiv(int(slate::ceildiv(nlocA, nb)), (lcm / nprow));
-        int lwork = std::max(n, 2*mlocA + nlocA + ldw);
-        std::vector<real_t> worknorm(lwork);
-        real_t C_norm = scalapack_plange(
-                            "One", n, n, &C_chk[0], ione, ione, descC_chk, &worknorm[0]);
-
-        real_t A_inv_norm = scalapack_planhe(
-                                "One", uplo2str(A.uplo()),
-                                n, &A_tst[0], ione, ione, descA_tst, &worknorm[0]);
-
-        double residual = C_norm / (A_norm * n * A_inv_norm);
-        params.error() = residual;
-
+        slate::Matrix<scalar_t> X, Y;
+        X = slate::Matrix<scalar_t>( n, nrhs, nb, p, q, MPI_COMM_WORLD);
+        Y = slate::Matrix<scalar_t>( n, nrhs, nb, p, q, MPI_COMM_WORLD);
+        slate::Target origin_target = origin2target(origin);
+        X.insertLocalTiles(origin_target);
+        Y.insertLocalTiles(origin_target);
+        generate_matrix( params.matrixB, X );  // rand
+        blas::real_type<scalar_t> normX, error;
+        normX = slate::norm( slate::Norm::One, X );
+        slate::multiply(  one, Aref, X, zero, Y );  // hemm: Y = Aref X;
+        slate::multiply( -one, A, Y, one, X );  // hemm: X = X - A Y
+        error = slate::norm( slate::Norm::One, X ) / (n * normX);
+        params.error() = error;
         real_t tol = params.tol() * std::numeric_limits<real_t>::epsilon();
         params.okay() = (params.error() <= tol);
     }
 
+#if 0
+    if (check) {
+        #ifdef SLATE_HAVE_SCALAPACK
+
+            // BLACS/MPI variables
+            int ictxt, p_, q_, myrow_, mycol_, info;
+            int A_desc[9], Aref_desc[9];
+            int Cchk_desc[9];
+            int mpi_rank_ = 0, nprocs = 1;
+
+            // initialize BLACS and ScaLAPACK
+            Cblacs_pinfo(&mpi_rank_, &nprocs);
+            slate_assert( mpi_rank == mpi_rank_ );
+            slate_assert(p*q <= nprocs);
+            Cblacs_get(-1, 0, &ictxt);
+            Cblacs_gridinit(&ictxt, "Col", p, q);
+            Cblacs_gridinfo(ictxt, &p_, &q_, &myrow_, &mycol_);
+            slate_assert( p == p_ );
+            slate_assert( q == q_ );
+            slate_assert( myrow == myrow_ );
+            slate_assert( mycol == mycol_ );
+
+            scalapack_descinit(A_desc, n, n, nb, nb, izero, izero, ictxt, mlocA, &info);
+            slate_assert(info == 0);
+
+            scalapack_descinit(Aref_desc, n, n, nb, nb, izero, izero, ictxt, mlocA, &info);
+            slate_assert(info == 0);
+
+            scalapack_descinit(Cchk_desc, n, n, nb, nb, izero, izero, ictxt, mlocA, &info);
+            slate_assert(info == 0);
+
+            // Check  || I - inv(A)*A || / ( || A || * N ) <=  tol * eps
+
+            if (origin != slate::Origin::ScaLAPACK) {
+                // Copy SLATE result back from GPU or CPU tiles.
+                copy(A, &A_data[0], A_desc);
+            }
+
+            // make diagonal real
+            for (int64_t i = 0; i < Aref.nt(); ++i) {
+                if (Aref.tileIsLocal(i, i)) {
+                    auto T = Aref(i, i);
+                    for (int ii = 0; ii < Aref.tileMb(i); ++ii) {
+                        T.at(ii, ii) = std::real( T.at(ii, ii) );
+                    }
+                }
+            }
+
+            // Make Cchk_data into an identity matrix to check the result of
+            // multiplying A and A_inv.
+            // Cchk_data starts with the same size/dimensions as A_data.
+            std::vector<scalar_t> Cchk_data( A_data.size() );
+            scalar_t zero = 0.0; scalar_t one = 1.0;
+            scalapack_plaset("All", n, n, zero, one, &Cchk_data[0], ione, ione, Cchk_desc);
+
+            // Cchk_data has been setup as an identity matrix; Cchk_data = C_chk - inv(A)*A
+            // A should have real diagonal. potrf and potri ignore the img part on the diagonal
+            scalar_t alpha = -1.0; scalar_t beta = 1.0;
+            scalapack_phemm("Left", uplo2str(uplo), n, n, alpha,
+                            &A_data[0], ione, ione, A_desc,
+                            &Aref_data[0], ione, ione, Aref_desc, beta,
+                            &Cchk_data[0], ione, ione, Cchk_desc);
+
+            // Norm of Cchk_data ( = I - inv(A) * A )
+            // allocate work space for lange and lanhe
+            int lcm = scalapack_ilcm(&p, &q);
+            int ldw = nb*slate::ceildiv(int(slate::ceildiv(nlocA, nb)), (lcm / p));
+            int lwork = std::max(n, 2*mlocA + nlocA + ldw);
+            std::vector<real_t> worknorm(lwork);
+            real_t C_norm = scalapack_plange(
+                                "One", n, n, &Cchk_data[0], ione, ione, Cchk_desc, &worknorm[0]);
+
+            real_t A_inv_norm = scalapack_planhe(
+                                    "One", uplo2str(A.uplo()),
+                                    n, &A_data[0], ione, ione, A_desc, &worknorm[0]);
+
+            double residual = C_norm / (A_norm * n * A_inv_norm);
+            params.error() = residual;
+
+            real_t tol = params.tol() * std::numeric_limits<real_t>::epsilon();
+            params.okay() = (params.error() <= tol);
+
+            Cblacs_gridexit(ictxt);
+            //Cblacs_exit(1) does not handle re-entering
+        #else
+            if (mpi_rank == 0)
+                printf( "ScaLAPACK not available\n" );
+        #endif
+    }
+#endif
+
     if (ref) {
         // todo: call to reference potri from ScaLAPACK not implemented
     }
-
-    Cblacs_gridexit(ictxt);
-    //Cblacs_exit(1) does not handle re-entering
 }
 
 // -----------------------------------------------------------------------------

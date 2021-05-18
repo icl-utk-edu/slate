859efbd6ad7dfc3efc190701676d2e0a0d8987fb
Mark Gates <mgates3@icl.utk.edu>
2021-05-18 14:24:29 -0400

    Version 2021.05.02

--- include/slate/slate.hh
+++ include/slate/slate.hh
@@ -24,8 +24,8 @@
 namespace slate {
 
 // Version is updated by make_release.py; DO NOT EDIT.
-// Version 2021.05.01
-#define SLATE_VERSION 20210501
+// Version 2021.05.02
+#define SLATE_VERSION 20210502
 
 int version();
 const char* id();

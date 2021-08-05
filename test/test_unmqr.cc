8ff00106dbc1fd3680b9042259ef98c5f7d8096b
Kadir Akbudak <kadir.cs@gmail.com>
2021-08-05 07:53:11 -0400

    use slate matrix generator,remove includes

--- test/test_unmqr.cc
+++ test/test_unmqr.cc
@@ -3,34 +3,26 @@
 // This program is free software: you can redistribute it and/or modify it under
 // the terms of the BSD 3-Clause license. See the accompanying LICENSE file.
 
-#include "slate/slate.hh"
 #include "test.hh"
-#include "blas/flops.hh"
 #include "lapack/flops.hh"
 #include "print_matrix.hh"
-
-#include "scalapack_wrappers.hh"
-#include "scalapack_support_routines.hh"
-#include "scalapack_copy.hh"
-
-#include <cmath>
-#include <cstdio>
-#include <cstdlib>
-#include <utility>
-
+#include "grid_utils.hh"
 //------------------------------------------------------------------------------
 template <typename scalar_t>
 void test_unmqr_work(Params& params, bool run)
 {
     using real_t = blas::real_type<scalar_t>;
     using blas::real;
-    // using llong = long long;
+
+    // Constants
+    const scalar_t zero = 0;
+    const scalar_t one = 1;
 
     // get & mark input values
     int64_t m = params.dim.m();
     int64_t n = params.dim.n();
-    int64_t p = params.grid.m();
-    int64_t q = params.grid.n();
+    int p = params.grid.m();
+    int q = params.grid.n();
     int64_t nb = params.nb();
     int64_t ib = params.ib();
     int64_t lookahead = params.lookahead();
@@ -40,6 +32,7 @@ void test_unmqr_work(Params& params, bool run)
     int verbose = params.verbose();
     slate::Origin origin = params.origin();
     slate::Target target = params.target();
+    params.matrix.mark();
 
     // mark non-standard output values
     params.time();
@@ -50,85 +43,64 @@ void test_unmqr_work(Params& params, bool run)
     if (! run)
         return;
 
-    // Local values
-    const scalar_t zero = 0;
-    const scalar_t one = 1;
+    slate::Options const opts =  {
+        {slate::Option::Lookahead, lookahead},
+        {slate::Option::Target, target},
+        {slate::Option::MaxPanelThreads, panel_threads},
+        {slate::Option::InnerBlocking, ib}
+    };
 
-    // BLACS/MPI variables
-    int ictxt, nprow, npcol, myrow, mycol, info;
-    int descA_tst[9], descA_ref[9], descQR_tst[9];
-    int iam = 0, nprocs = 1;
-    int iseed = 1;
+    // MPI variables
+    int mpi_rank, myrow, mycol;
+    MPI_Comm_rank(MPI_COMM_WORLD, &mpi_rank);
+    gridinfo(mpi_rank, p, q, &myrow, &mycol);
 
-    // initialize BLACS and ScaLAPACK
-    Cblacs_pinfo(&iam, &nprocs);
-    slate_assert(p*q <= nprocs);
-    Cblacs_get(-1, 0, &ictxt);
-    Cblacs_gridinit(&ictxt, "Col", p, q);
-    Cblacs_gridinfo(ictxt, &nprow, &npcol, &myrow, &mycol);
-
-    // matrix A, figure out local size, allocate, create descriptor, initialize
-    int64_t mlocA = scalapack_numroc(m, nb, myrow, 0, nprow);
-    int64_t nlocA = scalapack_numroc(n, nb, mycol, 0, npcol);
-    scalapack_descinit(descA_tst, m, n, nb, nb, 0, 0, ictxt, mlocA, &info);
-    slate_assert(info == 0);
-    int64_t lldA = (int64_t)descA_tst[8];
-    std::vector<scalar_t> A_tst(lldA*nlocA);
-    scalapack_pplrnt(&A_tst[0], m, n, nb, nb, myrow, mycol, nprow, npcol, mlocA, iseed + 1);
-
-    // matrix QR, for unmqr
-    std::vector<scalar_t> QR_tst(1);
-    scalapack_descinit(descQR_tst, m, n, nb, nb, 0, 0, ictxt, mlocA, &info);
-    slate_assert(info == 0);
+    // Matrix A: figure out local size.
+    int64_t mlocA = num_local_rows_cols(m, nb, myrow, p);
+    int64_t nlocA = num_local_rows_cols(n, nb, mycol, q);
+    int64_t lldA  = blas::max(1, mlocA); // local leading dimension of A
+    std::vector<scalar_t> A_data;
 
     slate::Matrix<scalar_t> A;
+    slate::Target origin_target = origin2target(origin);
     if (origin != slate::Origin::ScaLAPACK) {
-        // Copy local ScaLAPACK data to GPU or CPU tiles.
-        slate::Target origin_target = origin2target(origin);
-        A = slate::Matrix<scalar_t>(m, n, nb, nprow, npcol, MPI_COMM_WORLD);
+        // SLATE allocates CPU or GPU tiles.
+        A = slate::Matrix<scalar_t>(m, n, nb, p, q, MPI_COMM_WORLD);
         A.insertLocalTiles(origin_target);
-        copy(&A_tst[0], descA_tst, A);
     }
     else {
-        // create SLATE matrices from the ScaLAPACK layouts
-        A = slate::Matrix<scalar_t>::fromScaLAPACK(m, n, &A_tst[0], lldA, nb, nprow, npcol, MPI_COMM_WORLD);
+        // create SLATE matrix from the ScaLAPACK layouts
+        A_data.resize( lldA * nlocA );
+        A = slate::Matrix<scalar_t>::fromScaLAPACK( m, n, &A_data[0], lldA, nb, p, q, MPI_COMM_WORLD );
     }
-    slate::TriangularFactors<scalar_t> T;
 
+    slate::generate_matrix(params.matrix, A);
     if (verbose > 1) {
         print_matrix("A", A);
     }
 
-    // copy test data and create a descriptor for unmqr
-    std::vector<scalar_t> A_ref;
+    slate::TriangularFactors<scalar_t> T;
+    // For checks, keep copy of original matrix A.
     slate::Matrix<scalar_t> Aref;
+    std::vector<scalar_t> Aref_data;
+    real_t A_norm = 0; //initialize to prevent compiler warning
     if (check) {
-        A_ref = A_tst;
-        scalapack_descinit(descA_ref, m, n, nb, nb, 0, 0, ictxt, mlocA, &info);
-        slate_assert(info == 0);
+        // Norm of original matrix: || A ||_1
+        A_norm = slate::norm( slate::Norm::One, A, opts );
 
+        // For simplicity, always use ScaLAPACK format for Aref.
+        Aref_data.resize( lldA * nlocA );
         Aref = slate::Matrix<scalar_t>::fromScaLAPACK(
-                m, n, &A_ref[0], lldA, nb, nprow, npcol, MPI_COMM_WORLD);
+                   m, n, &Aref_data[0], lldA, nb, p, q, MPI_COMM_WORLD);
+        slate::copy(A, Aref);
     }
 
     //==================================================
     // Run SLATE test.
     //==================================================
-    slate::qr_factor(A, T, {
-        {slate::Option::Lookahead, lookahead},
-        {slate::Option::Target, target},
-        {slate::Option::MaxPanelThreads, panel_threads},
-        {slate::Option::InnerBlocking, ib}
-    });
-
-    //---------------------
+    slate::qr_factor(A, T, opts);
     // Using traditional BLAS/LAPACK name
-    // slate::geqrf(A, T, {
-    //     {slate::Option::Lookahead, lookahead},
-    //     {slate::Option::Target, target},
-    //     {slate::Option::MaxPanelThreads, panel_threads},
-    //     {slate::Option::InnerBlocking, ib}
-    // });
+    // slate::geqrf(A, T, opts);
 
     if (verbose > 1) {
         print_matrix("A_factored", A);
@@ -144,47 +116,22 @@ void test_unmqr_work(Params& params, bool run)
     //      || A ||_1 * m
     //
     //==================================================
+    // R is the upper part of A matrix.
+    slate::TrapezoidMatrix<scalar_t> R(slate::Uplo::Upper, slate::Diag::NonUnit, A);
 
-    if (origin != slate::Origin::ScaLAPACK) {
-        // Copy SLATE result back from GPU or CPU tiles.
-        copy(A, &A_tst[0], descA_tst);
-    }
+    std::vector<scalar_t> QR_data(Aref_data.size(), zero);
+    slate::Matrix<scalar_t> QR = slate::Matrix<scalar_t>::fromScaLAPACK(
+                                     m, n, &QR_data[0], lldA, nb, p, q, MPI_COMM_WORLD);
+    QR.insertLocalTiles(origin_target);
 
-    // Norm of original matrix: || A ||_1
-    real_t A_norm = 0.0;
-    if (check) {
-        A_norm = slate::norm(slate::Norm::One, Aref);
-    }
+    // R1 is the upper part of QR matrix.
+    slate::TrapezoidMatrix<scalar_t> R1(slate::Uplo::Upper, slate::Diag::NonUnit, QR);
 
-    // Zero out QR, then copy R, stored in upper triangle of A_tst.
-    // todo: replace with slate set/copy functions.
-    QR_tst = std::vector<scalar_t>(A_tst.size(), zero);
-    scalapack_placpy("Upper", std::min(m, n), n,
-                     &A_tst[0], 1, 1, descA_tst,
-                     &QR_tst[0], 1, 1, descQR_tst);
-    if (! check) {
-        A_tst.clear();
-    }
-
-    slate::Matrix<scalar_t> QR;
-    if (origin != slate::Origin::ScaLAPACK) {
-        // Copy local ScaLAPACK data to GPU or CPU tiles.
-        slate::Target origin_target = origin2target(origin);
-        QR = slate::Matrix<scalar_t>(m, n, nb, nprow, npcol, MPI_COMM_WORLD);
-        QR.insertLocalTiles(origin_target);
-        copy(&QR_tst[0], descQR_tst, QR);
-    }
-    else {
-        QR = slate::Matrix<scalar_t>::fromScaLAPACK(
-                m, n, &QR_tst[0], lldA, nb, nprow, npcol, MPI_COMM_WORLD);
-    }
+    // Copy A's upper trapezoid R to QR's upper trapezoid R1.
+    slate::copy(R, R1);
     
     if (verbose > 1) {
         print_matrix("R", QR);
-    }
-    
-    if (! check) {
-        QR_tst.clear();
     }
     
     if (! check) {
@@ -203,16 +150,14 @@ void test_unmqr_work(Params& params, bool run)
     }
     double time = testsweeper::get_wtime();
 
+    // Multiply QR by Q (implicitly stored in A and T).
     // Form QR, where Q's representation is in A and T, and R is in QR.
     slate::qr_multiply_by_q(
-        slate::Side::Left, slate::Op::NoTrans, A, T, QR,
-        {{slate::Option::Target, target}}
-    );
-    //---------------------
+        slate::Side::Left, slate::Op::NoTrans, A, T, QR, opts);
     // Using traditional BLAS/LAPACK name
-    // slate::unmqr(slate::Side::Left, slate::Op::NoTrans, A, T, QR, {
-    //     {slate::Option::Target, target}
-    // });
+    // slate::unmqr(
+    //     slate::Side::Left, slate::Op::NoTrans, A, T, QR, opts);
+
     {
         slate::trace::Block trace_block("MPI_Barrier");
         MPI_Barrier(MPI_COMM_WORLD);
@@ -231,19 +176,12 @@ void test_unmqr_work(Params& params, bool run)
     }
 
     if (check) {
-        if (origin != slate::Origin::ScaLAPACK) {
-            copy(QR, &QR_tst[0], descQR_tst);
-        }
-        
+        // QR should now have the product Q*R, which should equal the original A.
+        // Subtract the original Aref from QR.
         // Form QR - A, where A is in Aref.
-        // todo: slate::geadd(-one, Aref, QR);
-        // using axpy assumes A_ref and QR_tst have same lda.
-        blas::axpy(QR_tst.size(), -one, &A_ref[0], 1, &QR_tst[0], 1);
-        
-        if (origin != slate::Origin::ScaLAPACK) {
-            copy(&QR_tst[0], descQR_tst, QR);
-        }
-
+        // todo: slate::add(-one, Aref, QR);
+        // using axpy assumes Aref_data and QR_data have same lda.
+        blas::axpy(QR_data.size(), -one, &Aref_data[0], 1, &QR_data[0], 1);
         if (verbose > 1) {
             print_matrix("QR - A", QR);
         }
@@ -256,9 +194,6 @@ void test_unmqr_work(Params& params, bool run)
         real_t tol = params.tol() * 0.5 * std::numeric_limits<real_t>::epsilon();
         params.okay() = (params.error() <= tol);
     }
-
-    Cblacs_gridexit(ictxt);
-    //Cblacs_exit(1) does not handle re-entering
 }
 
 // -----------------------------------------------------------------------------

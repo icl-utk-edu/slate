d050a33d6f736d4b783b9bd22dc39398da74d985
Mark Gates <mgates3@icl.utk.edu>
2021-08-31 14:58:04 -0400

    update run_tests with unmtr

--- test/run_tests.py
+++ test/run_tests.py
@@ -477,30 +477,25 @@ if (opts.rq):
 # symmetric/Hermitian eigenvalues
 if (opts.syev):
     cmds += [
-    # todo nb, uplo, jobz
+    # todo: uplo, jobz
     [ 'heev',  gen + dtype + la + n ],
     #[ 'ungtr', gen + dtype + la + n + uplo ],
-    #[ 'unmtr', gen + dtype_real    + la + mn + uplo + side + trans    ],  # real does trans = N, T, C
-    #[ 'unmtr', gen + dtype_complex + la + mn + uplo + side + trans_nc ],  # complex does trans = N, C, not T
-    # todo nb, uplo, origin
-    [ 'unmtr_he2hb', target + grid + check + ref + tol + repeat + dtype_real    + ' --nb 50' + ' --origin s' + side + trans    ],  # real does trans = N, T, C
-    # todo: include (side=l and trans=c) as well as (side=r and trans=n)
-    # [ 'unmtr_he2hb', target + p + q + check + ref + tol + repeat + dtype_complex + ' --nb 50' + ' --origin s' + side + trans_nc ],  # complex does trans = N, C, not T
-    [ 'unmtr_he2hb', target + grid + check + ref + tol + repeat + dtype_complex + ' --nb 50' + ' --origin s' + ' --side l' + ' --trans n' ],
-    [ 'unmtr_he2hb', target + grid + check + ref + tol + repeat + dtype_complex + ' --nb 50' + ' --origin s' + ' --side r' + ' --trans c' ],
-    # todo nb, uplo
+
+    # todo uplo, nk
+    [ 'unmtr_he2hb', gen + dtype_real    + side + trans    + n ],  # real does trans = N, T, C
+    [ 'unmtr_he2hb', gen + dtype_complex + side + trans_nc + n ],  # complex does trans = N, C
+
+    # todo: uplo, side, trans, nk
+    [ 'unmtr_hb2st', gen_no_target + dtype_real    + n ],
+    [ 'unmtr_hb2st', gen_no_target + dtype_complex + n ],
+
+    # todo: uplo
     [ 'he2hb', gen_no_target + dtype + n ],
     [ 'hb2st', gen_no_target + dtype + n ],
+
     # sterf doesn't take origin, target, nb, uplo
     [ 'sterf',  grid + check + ref + tol + repeat + dtype + n ],
     [ 'steqr2', grid + check + ref + tol + repeat + dtype + n ],
-    # todo: hb2st
-
-    # Banded
-    #[ 'hbev',  gen + dtype + la + n + jobz + uplo ],
-    #[ 'ubgtr', gen + dtype + la + n + uplo ],
-    #[ 'ubmtr', gen + dtype_real    + la + mn + uplo + side + trans    ],
-    #[ 'ubmtr', gen + dtype_complex + la + mn + uplo + side + trans_nc ],
     ]
 
 # generalized symmetric/Hermitian eigenvalues

c839ae7ec7f00f2de589362e9a1cd98d632826c3
Paul Bagwell <pdbagwell@icl.utk.edu>
2021-11-01 18:00:05 -0400

    getri, posv

--- test/test_getri.cc
+++ test/test_getri.cc
@@ -71,8 +71,12 @@ void test_getri_work(Params& params, bool run)
     int64_t mlocA = num_local_rows_cols(n, nb, myrow, p);
     int64_t nlocA = num_local_rows_cols(n, nb, mycol, q);
     int64_t lldA  = blas::max(1, mlocA); // local leading dimension of A
-    std::vector<scalar_t> A_data(lldA*nlocA);
 
+    // Allocate ScaLAPACK data if needed.
+    std::vector<scalar_t> A_data;
+    if (check || ref) {
+        A_data.resize( lldA * nlocA );
+    }
     // todo: work-around to initialize BaseMatrix::num_devices_
     slate::Matrix<scalar_t> A0(n, n, nb, p, q, MPI_COMM_WORLD);
 

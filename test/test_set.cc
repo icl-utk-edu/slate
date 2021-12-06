dced60b753d51dd1b14c5b46fcbefe4e483bee29
Daniel Bielich <drbieli@saturn.icl.utk.edu>
2021-12-06 11:55:54 -0500

    Resolving conflicts

--- test/test_set.cc
+++ test/test_set.cc
@@ -54,7 +54,6 @@ void test_set_work(Params& params, bool run)
     bool ref = params.ref() == 'y' || ref_only;
     bool check = params.check() == 'y' && ! ref_only;
     bool trace = params.trace() == 'y';
-    //int verbose = params.verbose();
     slate::Origin origin = params.origin();
     slate::Target target = params.target();
     params.matrix.mark();
@@ -190,7 +189,7 @@ void test_set_work(Params& params, bool run)
         // compute and save timing/performance
         params.time() = time;
 
-        //print_matrix("A", A, params);
+        print_matrix("A", A, params);
 
     }
 
@@ -235,12 +234,12 @@ void test_set_work(Params& params, bool run)
 
             time = barrier_get_wtime(MPI_COMM_WORLD) - time;
 
-            //print_matrix("Aref", mlocA, nlocA, &Aref_data[0], lldA, p, q, MPI_COMM_WORLD, params);
+            print_matrix("Aref", mlocA, nlocA, &Aref_data[0], lldA, p, q, MPI_COMM_WORLD, params);
 
             // get differences A = A - Aref
             slate::add(-one, Aref, one, A);
 
-            //print_matrix("Diff", A, params);
+            print_matrix("Diff", A, params);
 
             // norm(A - Aref)
             real_t diff_norm = slate::norm(slate::Norm::One, A);

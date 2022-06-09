1cf65da22be6c7e0c4cf4ad7418fa5e8a982c4e7
Asim YarKhan <yarkhan@icl.utk.edu>
2022-06-09 16:46:42 -0400

    Add a _OPENMP version check, add omp_set_nested(1) if OpenMP<5.0.

--- include/slate/internal/OmpSetMaxActiveLevels.hh
+++ include/slate/internal/OmpSetMaxActiveLevels.hh
@@ -35,6 +35,10 @@ public:
     ///     Ensure that OpenMP max-active-levels-var ICV has this minimum.
     OmpSetMaxActiveLevels(int min_active_levels)
     {
+        #if defined(_OPENMP) && _OPENMP < 201811
+            // if OpenMP version < 5.0 then enable omp_set_nested
+            omp_set_nested(1);
+        #endif
         int curr_max_active_levels = omp_get_max_active_levels();
         if (min_active_levels > curr_max_active_levels) {
             // record the original value

b4c5ddd2422679eea7a01b402a218d882afd0b54
Gerald Ragghianti <ragghianti@icl.utk.edu>
2022-06-20 11:16:01 -0400

    Set up correct version of blacs

--- .github/workflows/setup_nogpu.sh
+++ .github/workflows/setup_nogpu.sh
@@ -3,6 +3,7 @@
 section "======================================== Load openmpi"
 module load openmpi
 export OMPI_CXX=${CXX}
+echo "mkl_blacs = openmpi" >> make.inc
 
 section "======================================== Configure NoGPU"
 echo "gpu_backend = none" >> make.inc

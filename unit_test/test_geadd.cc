b9a690c25c308172d95d6af982d15ad84a499fe0
scayrols <sebastien.cayrols@icl.utk.edu>
2022-09-22 15:54:05 -0400

    Add first batch version of reduce

--- unit_test/test_geadd.cc
+++ unit_test/test_geadd.cc
@@ -561,16 +561,429 @@ void run_tests_geadd_batch_device()
 }
 
 //------------------------------------------------------------------------------
+#define Aarray_row_major 1
+// reduce_count is 1 tile of A and reduce_count -1 tiles of B
+template <typename scalar_t>
+void test_reduce_batch_dev_worker(
+    int m, int n, int lda,
+    scalar_t alpha, scalar_t beta,
+    int batch_count, int reduce_count)
+{
+    using real_t = blas::real_type<scalar_t>;
+
+    if (num_devices == 0) {
+        test_skip("requires num_devices > 0");
+    }
+    if (verbose > 2)
+        printf( "\n" );
+
+    real_t eps = std::numeric_limits<real_t>::epsilon();
+    int ldb = lda;
+    std::vector< slate::Tile< scalar_t > > list_A(  0 );
+    std::vector< slate::Tile< scalar_t > > list_dA( 0 );
+    std::vector< slate::Tile< scalar_t > > list_B(  0 );
+    std::vector< slate::Tile< scalar_t > > list_dB( 0 );
+    std::vector< slate::Tile< scalar_t > > list_B0( 0 );
+    std::vector< slate::Tile< scalar_t > > list_C0( 0 );
+    int64_t idist = 2;
+    int64_t iseed[4] = { 0, 1, 2, 3 };
+
+    // Create a list of A on the Host and setup with random data
+    for (int m_j = 0; m_j < batch_count; ++m_j) {
+        for (int m_i = 0; m_i < reduce_count - 1; ++m_i) {
+            scalar_t* tmp_data = new scalar_t[ lda * n ];
+            test_assert( tmp_data != nullptr );
+            list_A.push_back(
+                    slate::Tile<scalar_t>( m, n, tmp_data, lda,
+                        slate::HostNum, slate::TileKind::UserOwned ) );
+            lapack::larnv( idist, iseed, lda * n, tmp_data );
+            if (verbose > 2)
+                for (int j = 0; j < n; ++j) {
+                    for (int i = 0; i < m; ++i) {
+                        printf(
+                                "batch %d [%d, %d]:"
+                                " A(%.8e + i%.8e)\n",
+                                m_j, i, j,
+                                std::real( tmp_data[ i + j*lda ] ),
+                                std::imag( tmp_data[ i + j*lda ] ) );
+                    }
+                }
+        }
+    }
+
+    // Create a list of B on the Host and setup with random data.
+    // B is equivalent to a matrix
+    // of dimension (reduce_count - 1) by batch_count.
+    for (int m_j = 0; m_j < batch_count; ++m_j) {
+        scalar_t* tmp_data = new scalar_t[ ldb * n ];
+        test_assert( tmp_data != nullptr );
+        list_B.push_back(
+            slate::Tile<scalar_t>( m, n, tmp_data, ldb,
+                slate::HostNum, slate::TileKind::UserOwned ) );
+        lapack::larnv( idist, iseed, ldb * n, tmp_data );
+        if (verbose > 2)
+            for (int j = 0; j < n; ++j) {
+                for (int i = 0; i < m; ++i) {
+                    printf(
+                        "batch %d [%d, %d]:"
+                        " B(%.8e + i%.8e)\n",
+                        m_j, i, j,
+                        std::real( tmp_data[ i + j*lda ] ),
+                        std::imag( tmp_data[ i + j*lda ] ) );
+                }
+            }
+    }
+
+    // Create B0, a copy of B on the host
+    for (int m_j = 0; m_j < batch_count; ++m_j) {
+        scalar_t* tmp_data = new scalar_t[ ldb * n ];
+        test_assert( tmp_data != nullptr );
+        list_B0.push_back(
+            slate::Tile<scalar_t>( m, n, tmp_data, ldb,
+                slate::HostNum, slate::TileKind::UserOwned ) );
+        // Create tmp tiles in order to copy the content
+        auto T  = list_B[ m_j ];
+        auto T0 = list_B0[ m_j ];
+        slate::tile::gecopy( T, T0 );
+    }
+
+    // Create C0 on the Host, used later to check the result
+    for (int m_j = 0; m_j < batch_count; ++m_j) {
+        scalar_t* tmp_data = new scalar_t[ ldb * n ];
+        test_assert( tmp_data != nullptr );
+        list_C0.push_back(
+            slate::Tile<scalar_t>( m, n, tmp_data, ldb,
+                slate::HostNum, slate::TileKind::UserOwned ) );
+    }
+
+    // Create the queue
+    int device_idx;
+    blas::get_device( &device_idx );
+    const int batch_arrays_index = 0;
+    blas::Queue queue( device_idx, batch_arrays_index );
+
+    // Create list of A on device and copy data
+    for (int m_j = 0; m_j < batch_count; ++m_j) {
+        for (int m_i = 0; m_i < reduce_count - 1; ++m_i) {
+            scalar_t* dtmp_data;
+            dtmp_data = blas::device_malloc<scalar_t>(
+                    blas::max( lda * n, 1 ) );
+            test_assert( dtmp_data != nullptr );
+            list_dA.push_back(
+                    slate::Tile<scalar_t>( m, n, dtmp_data, lda,
+                        device_idx, slate::TileKind::UserOwned ) );
+            int m_ij = m_i + m_j * (reduce_count - 1);
+            list_A[ m_ij ].copyData( &list_dA[ m_ij ], queue );
+        }
+    }
+
+    // Create B on device and copy data
+    for (int m_j = 0; m_j < batch_count; ++m_j) {
+        scalar_t* dtmp_data;
+        dtmp_data = blas::device_malloc<scalar_t>( blas::max( ldb * n, 1 ) );
+        test_assert( dtmp_data != nullptr );
+        list_dB.push_back(
+                slate::Tile<scalar_t>( m, n, dtmp_data, ldb,
+                    device_idx, slate::TileKind::UserOwned ) );
+        list_B[ m_j ].copyData( &list_dB[ m_j ], queue );
+    }
+
+    // Create batch arrays
+    int64_t nAarray = batch_count * ( reduce_count - 1 );
+    scalar_t** Aarray = new scalar_t*[ nAarray ];
+    scalar_t** dAarray;
+    dAarray = blas::device_malloc<scalar_t*>( nAarray );
+    test_assert( dAarray != nullptr );
+#if Aarray_row_major
+    int cpt = 0;
+    for (int m_i = 0; m_i < reduce_count - 1; ++m_i) {
+        for (int m_j = 0; m_j < batch_count; ++m_j) {
+            int m_ij = m_i + m_j * ( reduce_count - 1 );
+            auto dA = list_dA[ m_ij ];
+            Aarray[ cpt++ ] = dA.data();
+            if (verbose > 2)
+                printf( "Aarray[%d] = A(%p)\n",
+                    m_ij, (void*) Aarray[ m_ij ] );
+        }
+    }
+#else
+    for (int m_j = 0; m_j < batch_count; ++m_j) {
+        for (int m_i = 0; m_i < reduce_count - 1; ++m_i) {
+            int m_ij = m_i + m_j * ( reduce_count - 1 );
+            auto dA = list_dA[ m_ij ];
+            Aarray[ m_ij ] = dA.data();
+            if (verbose > 2)
+                printf( "Aarray[%d] = A(%p)\n",
+                    m_ij, (void*) Aarray[ m_ij ] );
+        }
+    }
+#endif
+    blas::device_memcpy<scalar_t*>( dAarray, Aarray,
+                        nAarray,
+                        blas::MemcpyKind::HostToDevice,
+                        queue );
+
+    scalar_t** Barray = new scalar_t*[ batch_count ];
+    scalar_t** dBarray;
+    dBarray = blas::device_malloc<scalar_t*>( batch_count );
+    test_assert( dBarray != nullptr );
+    for (int m_j = 0; m_j < batch_count; ++m_j) {
+        auto dB = list_dB[ m_j ];
+        Barray[ m_j ] = dB.data();
+        if (verbose > 2)
+            printf( "Barray[%d] = B(%p)\n", m_j, (void*) Barray[ m_j ] );
+    }
+    blas::device_memcpy<scalar_t*>( dBarray, Barray,
+                        batch_count,
+                        blas::MemcpyKind::HostToDevice,
+                        queue );
+
+    // Add: B[k] = \beta * B[k] + \sum_{1}^{reduce_count} \alpha A[k]
+#if Aarray_row_major
+    slate::device::gereduce( m, n, reduce_count - 1,
+#else
+    slate::device::batch::reduce( m, n, reduce_count,
+#endif
+                          alpha, dAarray, lda,
+                          beta,  dBarray, ldb,
+                          batch_count, queue );
+
+    queue.sync();
+
+    // Copy the result back to the Host
+    for (int m_j = 0; m_j < batch_count; ++m_j) {
+        auto dB = list_dB[ m_j ];
+        auto B = list_B[ m_j ];
+        dB.copyData( &B, queue );
+        if (verbose > 2) {
+            auto Bdata = B.data();
+            for (int j = 0; j < n; ++j) {
+                for (int i = 0; i < m; ++i) {
+                    printf(
+                        "batch %d [%d, %d]:"
+                        " Bres(%.8e + i%.8e)\n",
+                        m_j, i, j,
+                        std::real( Bdata[ i + j*ldb ] ),
+                        std::imag( Bdata[ i + j*ldb ] ) );
+                }
+            }
+        }
+    }
+
+    // compute on CPU to check the results
+    for (int m_j = 0; m_j < batch_count; ++m_j) {
+        auto B0data = list_B0[ m_j ].data();
+        auto C0data = list_C0[ m_j ].data();
+        // Copy \alpha * A0 into C0
+        for (int j = 0; j < n; ++j) {
+            for (int i = 0; i < m; ++i) {
+                C0data[ i + j*ldb ] = beta * B0data[ i + j*ldb ];
+            }
+        }
+        // Iterate over B to accumulate into C0
+        for (int m_i = 0; m_i < reduce_count - 1; ++m_i) {
+            auto Adata  = list_A[ m_j * ( reduce_count - 1 ) + m_i ].data();
+            for (int j = 0; j < n; ++j) {
+                for (int i = 0; i < m; ++i) {
+                    C0data[ i + j*ldb ] += alpha * Adata[ i + j*lda ];
+                }
+            }
+        }
+    }
+
+    // Create a vector from \alpha and \beta
+    // XXX
+  //std::vector<scalar_t> cst(reduce_count * 2) = {alpha};
+  //cst[ reduce_count - 1 ] = cst[ 2 * reduce_count - 1 ] = beta;
+  //real_t max_csts = lapack::lange(
+  //        lapack::Norm::Max, reduce_count * 2, 1, cst.data(), 1 );
+    real_t max_csts = std::abs( alpha );
+
+    for (int m_j = 0; m_j < batch_count; ++m_j) {
+        auto A  = list_A[ m_j * ( reduce_count - 1 ) ];
+        auto B  = list_B[ m_j ];
+        auto dB = list_dB[ m_j ];
+        scalar_t *Bdata   = B.data(); // Overwritten
+        scalar_t *C0data  = list_C0[ m_j ].data(); // Computed on the Host
+
+        // Get max values in order to check the error
+        real_t max_A = lapack::lange(
+            lapack::Norm::Max, A.mb(), A.nb(), A.data(), A.stride() );
+        for (int m_i = 1; m_i < reduce_count - 1; ++m_i) {
+            A = list_A[ m_i + m_j * ( reduce_count - 1 ) ];
+            max_A = std::max( max_A, lapack::lange(
+                                            lapack::Norm::Max, A.mb(),
+                                            A.nb(), A.data(), A.stride() ) );
+        }
+        real_t max_B = lapack::lange(
+            lapack::Norm::Max, B.mb(), B.nb(), B.data(), B.stride() );
+
+        //blas::axpy( B.size(), neg_one, B.data(), ione, C0.data(), ione );
+        for (int j = 0; j < n; ++j) {
+            for (int i = 0; i < m; ++i) {
+                Bdata[i + j*ldb] = Bdata[i + j*ldb] -  C0data[i + j*ldb];
+            }
+        }
+
+        // check final norm result
+        real_t result = lapack::lange(
+            lapack::Norm::Max, B.mb(), B.nb(), B.data(), B.stride() );
+
+        if (max_csts != scalar_t(0.0) && result != scalar_t(0.0))
+            result /= std::max( max_A, max_B ) * max_csts * reduce_count;
+
+        if (verbose) {
+            // Display (m, n, lda, alpha)//, beta)
+            if (m_j == 0)
+                printf(
+                    "\n(%4d, %4d, %4d, (%4.2f, %4.2f), (%4.2f, %4.2f), "
+                    "%2d, %2d:",
+                    m, n, lda,
+                    std::real( alpha ), std::imag( alpha ),
+                    std::real( beta ),  std::imag( beta ),
+                    batch_count, reduce_count );
+            if (verbose > 1)
+                printf( "\n\t[%d] error %.2e ", m_j, result );
+
+            if (verbose > 2 && result >= 3*eps) {
+                printf( "\n" );
+                for (int j = 0; j < n; ++j) {
+                    for (int i = 0; i < m; ++i) {
+                        printf(
+                            "batch %d [%d, %d]: "
+                            "Diff_host(%.8e + i%.8e), Diff_gpu(%.8e + i%.8e)\n",
+                            m_j, i, j,
+                            std::real( C0data[ i + j*ldb ] ),
+                            std::imag( C0data[ i + j*ldb ] ),
+                            std::real( Bdata[ i + j*ldb ] ),
+                            std::imag( Bdata[ i + j*ldb ] ) );
+                    }
+                }
+            }
+        }
+
+        blas::device_free( dB.data() );
+        for (int m_i = 0; m_i < reduce_count - 1; ++m_i) {
+            int64_t m_ij = m_i + m_j * ( reduce_count - 1 );
+            delete[] list_A[ m_ij ].data();
+            blas::device_free( list_dA[ m_ij ].data() );
+        }
+
+        delete[] Bdata;
+        delete[] C0data;
+
+        test_assert( result < 3*eps );
+    }
+
+    blas::device_free( dAarray );
+    blas::device_free( dBarray );
+    delete[] Aarray;
+    delete[] Barray;
+}
+
+template <typename scalar_t>
+void test_reduce_batch_dev()
+{
+    // Each tuple contains (mA, nA, lda)
+    std::list< std::tuple< int, int, int > > dims_list{
+            // Corner cases
+          //{   0,   0,   0 },
+          //{ 100,   0, 100 },
+          //{   0, 100,   0 },
+            // Square A matrix
+            {   1,   1,   1 },
+            {  10,  10,  10 },
+            {  20,  20,  20 },
+            {   7,   7,   7 },
+            {  11,   11, 11 },
+            {  33,   33, 33 },
+            // Rectangular A matrix
+            {   1, 100,   1 },
+            { 100,   1, 100 },
+            {  20,  30,  20 },
+            // lda != mA
+            {  20,  30,  roundup(20, 8) }, //as it was in the original test
+            {  33,  33,  50 },
+        };
+
+    // Each tuple contains (alpha, beta)
+    std::list<
+              std::tuple< std::complex<double>, std::complex<double> >
+             > values_list{
+            // Debug case
+            { {   1,   0 },
+              {   1,   0 } },
+            // All 0
+          //{ {   0,   0 },
+          //  {   0,   0 } },
+          //// Offdiag 0, diag != 0
+          //{ {   0,   0 },
+          //  { 0.5, 0.5 } },
+          //// Offdiag != 0, diag 0
+          //{ { 0.3, 0.3 },
+          //  {   0,   0 } },
+          //// Real != 0, Imag 0
+          //{ {   0, 0.3 },
+          //  {   0, 0.5 } },
+          //// Real = 0, Imag != 0
+          //{ { 0.3,   0 },
+          //  { 0.5,   0 } },
+          //// Standard case
+          //{ { 3.141592653589793, 1.414213562373095 },
+          //  { 2.718281828459045, 1.732050807568877 } },
+          //// Some negative values
+          //{ { -3.141592653589793, 1.414213562373095 },
+          //  { 2.718281828459045, -1.732050807568877 } },
+          //// Huge values
+          //{ { 3.141592653589793e8, 1.414213562373095 },
+          //  { 2.718281828459045e7, 1.732050807568877 } },
+        };
+
+    std::list< int > batch_count_list{ 2 };//, 3, 4, 5, 10, 20, 100 };
+    std::list< int > reduce_count_list{ 2 };//, 3, 4, 5, 10 };
+
+    for (auto dims : dims_list) {
+        int mA  = std::get<0>( dims );
+        int nA  = std::get<1>( dims );
+        int lda = std::get<2>( dims );
+        for (auto values : values_list) {
+            std::complex<double> alpha = std::get<0>( values );
+            std::complex<double> beta  = std::get<0>( values );
+            for (auto reduce_count : reduce_count_list)
+                for (auto batch_count : batch_count_list)
+                    test_reduce_batch_dev_worker<scalar_t>(
+                        mA, nA, lda,
+                        testsweeper::make_scalar<scalar_t>( alpha ),
+                        testsweeper::make_scalar<scalar_t>( beta ),
+                        batch_count, reduce_count );
+        }
+    }
+}
+
+template <typename... scalar_t>
+void run_tests_reduce_batch_device()
+{
+    ( run_test<scalar_t>(
+                          test_reduce_batch_dev<scalar_t>, "reduce_batch_dev" ),
+      ... );
+}
+
+
+//------------------------------------------------------------------------------
 /// Runs all tests. Called by unit test main().
 void run_tests()
 {
     if (mpi_rank == 0) {
         //-------------------- geadd_dev
-        run_tests_geadd_device<
-            float, double, std::complex<float>, std::complex<double>
-            >();
+      //run_tests_geadd_device<
+      //    float, double, std::complex<float>, std::complex<double>
+      //    >();
+      ////-------------------- geadd_batch_dev
+      //run_tests_geadd_batch_device<
+      //    float, double, std::complex<float>, std::complex<double>
+      //    >();
         //-------------------- geadd_batch_dev
-        run_tests_geadd_batch_device<
+        run_tests_reduce_batch_device<
             float, double, std::complex<float>, std::complex<double>
             >();
     }

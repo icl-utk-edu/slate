e916a20b4e84a13605092892fd426d2f53876e4d
Dalal Sukkari <sukkari@icl.utk.edu>
2023-03-02 10:39:45 -0500

    Added scalapack wrapper for DC eigensolver

--- test/scalapack_wrappers.hh
+++ test/scalapack_wrappers.hh
@@ -2095,6 +2095,117 @@ inline void scalapack_pstedc(
 // -----------------------------------------------------------------------------
 // -----------------------------------------------------------------------------
 
+#define scalapack_pssyevd BLAS_FORTRAN_NAME( pssyevd, PSSYEVD )
+#define scalapack_pdsyevd BLAS_FORTRAN_NAME( pdsyevd, PDSYEVD )
+#define scalapack_pcheevd BLAS_FORTRAN_NAME( pcheevd, PCHEEVD )
+#define scalapack_pzheevd BLAS_FORTRAN_NAME( pzheevd, PZHEEVD )
+
+extern "C" void scalapack_pssyevd(
+    const char* jobz, const char* uplo, blas_int* n,
+    float* A, blas_int* ia, blas_int* ja, blas_int* descA, float* W,
+    float* Z, blas_int* iz, blas_int* jz, blas_int* descZ,
+    float* work, blas_int* lwork, blas_int* iwork, blas_int* liwork,
+    blas_int* info);
+
+extern "C" void scalapack_pdsyevd(
+    const char* jobz, const char* uplo, blas_int* n,
+    double* A, blas_int* ia, blas_int* ja, blas_int* descA, double* W,
+    double* Z, blas_int* iz, blas_int* jz, blas_int* descZ,
+    double* work, blas_int* lwork, blas_int* iwork, blas_int* liwork,
+    blas_int* info);
+
+extern "C" void scalapack_pcheevd(
+    const char* jobz, const char* uplo, blas_int* n,
+    std::complex<float>* A, blas_int* ia, blas_int* ja, blas_int* descA, float* W,
+    std::complex<float>* Z, blas_int* iz, blas_int* jz, blas_int* descZ,
+    std::complex<float>* work, blas_int* lwork,
+    float* rwork, blas_int* lrwork, blas_int* iwork, blas_int* liwork,
+    blas_int* info);
+
+extern "C" void scalapack_pzheevd(
+    const char* jobz, const char* uplo, blas_int* n,
+    std::complex<double>* A, blas_int* ia, blas_int* ja, blas_int* descA, double* W,
+    std::complex<double>* Z, blas_int* iz, blas_int* jz, blas_int* descZ,
+    std::complex<double>* work, blas_int* lwork,
+    double* rwork, blas_int* lrwork, blas_int* iwork, blas_int* liwork,
+    blas_int* info);
+
+// -----------------------------------------------------------------------------
+
+inline void scalapack_pheevd(
+    const char* jobz, const char* uplo, blas_int* n,
+    float* A, blas_int* ia, blas_int* ja, blas_int* descA, float* W,
+    float* Z, blas_int* iz, blas_int* jz, blas_int* descZ,
+    float* work, blas_int* lwork,
+    float* rwork, blas_int* lrwork, blas_int* iwork, blas_int* liwork,
+    blas_int* info)
+{
+    scalapack_pssyevd(jobz, uplo, n, A, ia, ja, descA, W, Z, iz, jz, descZ, work, lwork, iwork, liwork, info);
+}
+
+inline void scalapack_pheevd(
+    const char* jobz, const char* uplo, blas_int* n,
+    double* A, blas_int* ia, blas_int* ja, blas_int* descA, double* W,
+    double* Z, blas_int* iz, blas_int* jz, blas_int* descZ,
+    double* work, blas_int* lwork,
+    double* rwork, blas_int* lrwork, blas_int* iwork, blas_int* liwork,
+    blas_int* info)
+{
+    scalapack_pdsyevd(jobz, uplo, n, A, ia, ja, descA, W, Z, iz, jz, descZ, work, lwork, iwork, liwork, info);
+}
+
+inline void scalapack_pheevd(
+    const char* jobz, const char* uplo, blas_int* n,
+    std::complex<float>* A, blas_int* ia, blas_int* ja, blas_int* descA, float* W,
+    std::complex<float>* Z, blas_int* iz, blas_int* jz, blas_int* descZ,
+    std::complex<float>* work, blas_int* lwork,
+    float* rwork, blas_int* lrwork, blas_int* iwork, blas_int* liwork,
+    blas_int* info)
+{
+    scalapack_pcheevd(jobz, uplo, n, A, ia, ja, descA, W, Z, iz, jz, descZ, work, lwork, rwork, lrwork, iwork, liwork, info);
+}
+
+inline void scalapack_pheevd(
+    const char* jobz, const char* uplo, blas_int* n,
+    std::complex<double>* A, blas_int* ia, blas_int* ja, blas_int* descA, double* W,
+    std::complex<double>* Z, blas_int* iz, blas_int* jz, blas_int* descZ,
+    std::complex<double>* work, blas_int* lwork,
+    double* rwork, blas_int* lrwork, blas_int* iwork, blas_int* liwork,
+    blas_int* info)
+{
+    scalapack_pzheevd(jobz, uplo, n, A, ia, ja, descA, W, Z, iz, jz, descZ, work, lwork, rwork, lrwork, iwork, liwork, info);
+}
+
+template <typename scalar_t>
+inline void scalapack_pheevd(
+    const char* jobz, const char* uplo,
+    int64_t n,
+    scalar_t* A, int64_t ia, int64_t ja, blas_int* descA,
+    blas::real_type<scalar_t>* W,
+    scalar_t* Z, int64_t iz, int64_t jz, blas_int* descZ,
+    scalar_t* work, int64_t lwork,
+    blas::real_type<scalar_t>* rwork, int64_t lrwork,
+    blas_int* iwork, int64_t liwork,
+    int64_t* info)
+{
+    blas_int n_     = int64_to_int(n);
+    blas_int ia_    = int64_to_int(ia);
+    blas_int ja_    = int64_to_int(ja);
+    blas_int iz_    = int64_to_int(iz);
+    blas_int jz_    = int64_to_int(jz);
+    blas_int lwork_ = int64_to_int(lwork);
+    blas_int lrwork_ = int64_to_int(lrwork);
+    blas_int liwork_ = int64_to_int(liwork);
+    blas_int info_  = int64_to_int(*info);
+    scalapack_pheevd(jobz, uplo, &n_, A, &ia_, &ja_, descA, W,
+                    Z, &iz_, &jz_, descZ,
+                    work, &lwork_, rwork, &lrwork_, iwork, &liwork_, &info_);
+    *info = (int64_t)info_;
+}
+
+// -----------------------------------------------------------------------------
+// -----------------------------------------------------------------------------
+
 #define scalapack_pslaset BLAS_FORTRAN_NAME( pslaset, PSLASET )
 #define scalapack_pdlaset BLAS_FORTRAN_NAME( pdlaset, PDLASET )
 #define scalapack_pclaset BLAS_FORTRAN_NAME( pclaset, PCLASET )

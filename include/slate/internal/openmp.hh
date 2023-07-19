5b63b700d26615e3db02101029fcf09933d8dfb7
Asim YarKhan <yarkhan@icl.utk.edu>
2023-07-19 19:56:57 +0000

    In SLATE openmp.hh, remove _OPENMP compiler guard and comment about -fsycl pass.

--- include/slate/internal/openmp.hh
+++ include/slate/internal/openmp.hh
@@ -9,45 +9,29 @@
 #ifndef SLATE_OPENMP_HH
 #define SLATE_OPENMP_HH
 
+// In OpenMP, using "#pragma omp default(none)" to make programmers
+// specify variable usage is good practice.  However some compilers
+// and libraries contain hidden variables that cause unexpected
+// warnings and errors.  SLATE uses a macro that can be defined at
+// compile time for debug building and testing.
+//
+// For example:  CXXFLAGS += -Dslate_omp_default_none='default(none)'
+//
 #ifndef slate_omp_default_none
 #define slate_omp_default_none
 #endif
 
-#ifdef _OPENMP
-    #include <omp.h>
-#else
-
-typedef int omp_lock_t;
-typedef int omp_nest_lock_t;
-
-#ifdef __cplusplus
-extern "C" {
-#endif
-
-int omp_get_initial_device();
-int omp_get_max_threads();
-int omp_get_num_devices();
-int omp_get_num_threads(void);
-int omp_get_thread_num(void);
-
-double omp_get_wtime();
-
-void omp_destroy_lock(omp_lock_t* lock);
-void omp_init_lock(omp_lock_t* lock);
-void omp_set_lock(omp_lock_t* lock);
-void omp_set_nested(int nested);
-void omp_unset_lock(omp_lock_t* lock);
-
-void omp_destroy_nest_lock(omp_nest_lock_t* lock);
-void omp_init_nest_lock(omp_nest_lock_t* lock);
-void omp_set_nest_lock(omp_nest_lock_t* lock);
-void omp_unset_nest_lock(omp_nest_lock_t* lock);
-
-#ifdef __cplusplus
-}
-#endif
-
-#endif // not _OPENMP
+// Include OpenMP headers
+//
+// Note: There is no _OPENMP guard because SLATE requires OpenMP and
+// checks for it at compile time.
+//
+// todo: On Intel SYCL platforms using both "-fsycl -fiopenmp" flags
+// can leave the _OPENMP macro undefined.  There are 2 sweeps over the
+// code, use ifdefs via __SYCL_DEVICE_ONLY__ macro to make code legal
+// for the SYCL pass.
+//
+#include <omp.h>
 
 // Defines a small class to wrap omp_set_max_active_levels()
 #include "slate/internal/OmpSetMaxActiveLevels.hh"

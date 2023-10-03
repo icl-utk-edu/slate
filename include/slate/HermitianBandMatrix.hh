536fa4ab4e8b91d79220fe0041435e5d262bad0b
Neil Lindquist <nlindqu1@icl.utk.edu>
2023-10-03 11:55:55 -0400

    Fix calls to tilePrepareToRecieve in *::gatherAll

--- include/slate/HermitianBandMatrix.hh
+++ include/slate/HermitianBandMatrix.hh
@@ -290,8 +290,7 @@ void HermitianBandMatrix<scalar_t>::gatherAll(std::set<int>& rank_set, int tag)
         for (int64_t i = istart; i <= iend; ++i) {
 
             // If receiving the tile.
-            this->storage_->tilePrepareToReceive( this->globalIndex( i, j ),
-                                                  Target::HostTask,
+            this->storage_->tilePrepareToReceive( this->globalIndex( i, j ), HostNum,
                                                   this->layout_ );
 
             // Send across MPI ranks.

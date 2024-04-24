1dfac6a700dcb5db3b194511c423845a7978c46f
G-Ragghianti <33492707+G-Ragghianti@users.noreply.github.com>
2024-04-24 13:53:01 -0400

    Fixing use of unused variables (#178)
    * Fixing use of unused variables
    
    * Adding in initialization of mpi_rank
    
    * changing to use of mpiRank()

--- test/test_copy.cc
+++ test/test_copy.cc
@@ -183,8 +183,6 @@ void test_copy_work(Params& params, bool run)
             Cblacs_gridexit(ictxt);
             //Cblacs_exit(1) does not handle re-entering
         #else  // not SLATE_HAVE_SCALAPACK
-            SLATE_UNUSED( A_norm );
-            SLATE_UNUSED( B_norm );
             if (A.mpiRank() == 0)
                 printf( "ScaLAPACK not available\n" );
         #endif
